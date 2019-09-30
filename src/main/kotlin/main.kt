3f5a2ecfd5c8269ae77b432825b86eb6c1ac4ab7
Atri Das <babinsamrat123@gmail.com>
2019-09-30 12:11:56 +0530

    initiates the blocking function

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,14 @@
 fun main(){
-    println("One")
-    println("Two")
-    println("Three")
-    // this is called synchronous code because it runs one after another
+    exampleBlocking()
 }
 
-fun delayedFunction(message:String){
+fun exampleBlocking() {
+    println("One")
+    printlnDelayed("Two")
+    println("Three")
+}
+
+fun printlnDelayed(message:String){
     Thread.sleep(1000)
     println(message)
 }
