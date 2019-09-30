7c1b772251a49b70333e14655cc6623852e79187
Atri Das <babinsamrat123@gmail.com>
2019-09-30 11:59:39 +0530

    creates the delay function

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,4 +2,10 @@ fun main(){
     println("One")
     println("Two")
     println("Three")
+    // this is called synchronous code because it runs one after another
+}
+
+fun delayedFunction(message:String){
+    Thread.sleep(1000)
+    println(message)
 }
